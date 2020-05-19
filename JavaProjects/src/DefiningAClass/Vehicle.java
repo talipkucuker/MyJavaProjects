14739d6b78426969e61e61d0a83dfd40a197b3e8
talipkucuker <talipkucuker@talips-mbp.attlocal.net>
2020-05-19 02:51:07 -0400

    Simple Define A Class

---
+++ JavaProjects/src/DefiningAClass/Vehicle.java
@@ -0,0 +1,9 @@
+package DefiningAClass;
+
+public class Vehicle {
+
+	int passengers; //number of passengers
+	int fuelcap; //fuel capacity in gallons
+	int mpg; //fuel consumption in miles per gallon
+}
+
